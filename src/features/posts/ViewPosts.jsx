7c87ec9b517c4014185e1132a6d78e79857cac5c
Joseph Wang <jjosephwwang@gmail.com>
2024-03-15 10:30:09 -0500

    Add classnames to homepage

--- src/features/posts/ViewPosts.jsx
+++ src/features/posts/ViewPosts.jsx
@@ -1,29 +1,37 @@
 import { useSelector } from "react-redux"
 import AddPostForm from "./AddPostForm"
 import ReactionButtons from "./ReactionButtons"
+import PostAuthor from "./PostAuthor"
+import TimeAgo from "./TimeAgo"
+import { Link } from "react-router-dom"
 
 const ViewPosts = () => {
   const posts = useSelector(state => state.posts)
 
-  console.log(posts)
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
   return (
     <>
       <AddPostForm />
-      {posts.map(post => <Post key={post.id} post={post}/>)}
+      {orderedPosts.map(post => <Post key={post.id} post={post}/>)}
     </>
   )
 }
 
 const Post = ({ post }) => {
   return (
-    <>
-      <p>{post.title}</p>
-      <p>{post.content}</p>
-      <p>{post.user}</p>
-      <p>{post.date}</p>
+    <article className="post-excerpt">
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content}</p>
       <ReactionButtons postId={post.id} reactions={post.reactions} />
-    </>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
   )
 }
 
